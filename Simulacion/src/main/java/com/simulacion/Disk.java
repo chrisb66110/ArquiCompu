3351a8c307024d3985a8befbeb3c7252c943adef
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 12:17:40 -0600

    Disk

---
+++ Simulacion/src/main/java/com/simulacion/Disk.java
@@ -0,0 +1,14 @@
+package com.simulacion;
+
+import java.util.BitSet;
+
+public class Disk {
+
+    public BitSet[] getBits(BitSet Address){
+        return null;
+    }
+
+    public void writeBits(BitSet Address, BitSet[] data){
+        
+    }
+}

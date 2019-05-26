456229d5ecfc60c29cdbb110e045a229b8a81c8d
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 12:10:59 -0600

    Bus

---
+++ Simulacion/src/main/java/com/simulacion/Bus.java
@@ -0,0 +1,22 @@
+package com.simulacion;
+
+import java.util.BitSet;
+
+public class Bus {
+    private BitSet lines;
+    private Object [] connectedDevices;
+    private EventsHandler eventHandler;
+
+    public void sendControl(BitSet controlCode, int device){
+
+    }
+
+    public void sendAddress(BitSet address, int device){
+
+    }
+
+    public void sendData(BitSet Data, int device){
+
+    }
+
+}

b5b2d2144a097919d60dd5a283e3401a7b181135
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 12:11:49 -0600

    ControlUnit

---
+++ Simulacion/src/main/java/com/simulacion/ControlUnit.java
@@ -0,0 +1,24 @@
+package com.simulacion;
+
+import java.util.BitSet;
+
+public class ControlUnit {
+    private BitSet programCounter;
+    private BitSet instructionRegister;
+    private CPUInterconnection internalBus;
+    private OperatingSystem os;
+    private EventsHandler eventHandler;
+
+    public void fetchNextInstruction(){
+
+    }
+
+    public void decodeInstruction(){
+
+    }
+
+    public void executeInstruciton(){
+
+    }
+
+}

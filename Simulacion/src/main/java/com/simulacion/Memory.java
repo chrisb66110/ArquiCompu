0e938d9a2f1e779a34a99dfd40c500c9d660de64
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 12:12:52 -0600

    Memory

---
+++ Simulacion/src/main/java/com/simulacion/Memory.java
@@ -0,0 +1,17 @@
+package com.simulacion;
+
+import java.util.BitSet;
+
+public class Memory {
+    private BitSet[] memory;
+    private EventsHandler eventHandler;
+    private Bus bus;
+
+    public BitSet[] getBits(int dir, OperandSize ammount){
+        return null;
+    }
+
+    public void writeBits(int dir, OperandSize ammount, BitSet[] data){
+
+    }
+}

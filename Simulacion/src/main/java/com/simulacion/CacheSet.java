52c2652c757b940ba871a4d0f93e62e3c9dd3f16
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 12:11:38 -0600

    CacheSet

---
+++ Simulacion/src/main/java/com/simulacion/CacheSet.java
@@ -0,0 +1,15 @@
+package com.simulacion;
+
+import java.util.BitSet;
+
+public class CacheSet {
+    private CacheBlock[] blocks;
+
+    public CacheSet(int blocks){
+
+    }
+
+    public BitSet find(BitSet address){
+        return null;
+    }
+}

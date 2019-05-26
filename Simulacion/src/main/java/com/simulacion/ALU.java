d32ff1abe413fb5cca4fef6eb295523423aad493
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 12:10:02 -0600

    ALU

---
+++ Simulacion/src/main/java/com/simulacion/ALU.java
@@ -0,0 +1,13 @@
+package com.simulacion;
+
+import java.util.BitSet;
+
+public class ALU {
+    public BitSet OperandA;
+    public BitSet OperandB;
+    public BitSet result;
+
+    public BitSet executeOperation(ALUOperations operation){
+        return null;
+    }
+}

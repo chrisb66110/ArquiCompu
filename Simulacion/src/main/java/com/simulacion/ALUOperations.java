df20ee0652cd4cd259697b4709f95225541baf95
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 12:10:41 -0600

    ALUOperations

---
+++ Simulacion/src/main/java/com/simulacion/ALUOperations.java
@@ -0,0 +1,19 @@
+package com.simulacion;
+
+public enum ALUOperations {
+    Add,
+    Substract,
+    Div,
+    Mul,
+    Remainder,
+    ShiftLeftA,
+    ShiftLeftL,
+    ShiftLeftC,
+    ShiftRightA,
+    ShiftRightL,
+    ShiftRightC,
+    And,
+    Or,
+    Xor,
+    Not
+}

44d0ca9cb98e7642360216ba367fb28957ecf2fa
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 12:12:32 -0600

    EventsHandler

---
+++ Simulacion/src/main/java/com/simulacion/EventsHandler.java
@@ -0,0 +1,13 @@
+package com.simulacion;
+
+public class EventsHandler {
+    private Events eventList;
+
+    public void addEvent(Events event){
+
+    }
+
+    public Events nextEvent(){
+        return null;
+    }
+}

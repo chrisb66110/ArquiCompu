74c59a1c48a651566beff5e8a5697b8d251afbc1
joe <rementeriajoseph@gmail.com>
2019-06-04 20:32:48 -0600

    comments

--- Simulacion/src/main/java/com/simulacion/CacheBlock.java
+++ Simulacion/src/main/java/com/simulacion/CacheBlock.java
@@ -1,9 +1,25 @@
+//-----------------------------------------------------------------------------
+// Package
 package com.simulacion;
-
+//-----------------------------------------------------------------------------
+// Imports
 import java.util.BitSet;
-
+//-----------------------------------------------------------------------------
+/**
+ * Class that emulates the behavior of a block inside a caché set
+ */
 public class CacheBlock {
+    //-------------------------------------------------------------------------
+    // Global Variables
     public BitSet data;
     public BitSet tag;
     public boolean valid;
+    //-------------------------------------------------------------------------
+    // Constructor
+    public CacheBlock() {
+        this.data = new BitSet();
+        this.tag = new BitSet();
+        this.valid = false;
+    }
+    //-------------------------------------------------------------------------
 }

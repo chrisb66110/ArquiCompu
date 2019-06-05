928477501687f4414e03f8e2fa851fc4cbe68cf4
joe <rementeriajoseph@gmail.com>
2019-06-04 19:53:01 -0600

    comments

--- Simulacion/src/main/java/com/simulacion/CacheSet.java
+++ Simulacion/src/main/java/com/simulacion/CacheSet.java
@@ -1,21 +1,38 @@
 //-----------------------------------------------------------------------------
-// Paquete
+// Package
 package com.simulacion;
 //-----------------------------------------------------------------------------
 // Imports
 import java.util.BitSet;
 //-----------------------------------------------------------------------------
 /**
+ * Class that emulates a set from an associative caché.
  *
+ * @author Joseph Rementería (b55824)
+ * @version 1.0
+ * @since 04-06-2019
  */
 public class CacheSet {
+    //-------------------------------------------------------------------------
+    // Global Variables
     private CacheBlock[] blocks;
-
+    //-------------------------------------------------------------------------
+    // Constructors
     public CacheSet(int blocks){
-
+        //---------------------------------------------------------------------
+        // Creation of the array.
+        this.blocks = new CacheBlock[blocks];
+        //---------------------------------------------------------------------
+        // Creation of each block
+        for (int index = 0; index < blocks; index++) {
+            this.blocks[index] = new CacheBlock();
+        }
+        //---------------------------------------------------------------------
     }
-
+    //-------------------------------------------------------------------------
+    // Methods
     public BitSet find(BitSet address){
         return null;
     }
+    //-------------------------------------------------------------------------
 }

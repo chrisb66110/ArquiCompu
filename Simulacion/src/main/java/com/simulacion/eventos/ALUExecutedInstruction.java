a4a130b983d1b88931607d5d5b0f0b4c72682f4b
christofer <christofer.rodriguez@ucr.ac.cr>
2019-06-07 19:06:32 -0600

    Documentacion de evento ALUExecutedInstruction

--- Simulacion/src/main/java/com/simulacion/eventos/ALUExecutedInstruction.java
+++ Simulacion/src/main/java/com/simulacion/eventos/ALUExecutedInstruction.java
@@ -1,5 +1,9 @@
 package com.simulacion.eventos;
 
+/**
+ * Evento para indicar que la ALU ya ejecutó la instrucción
+ * info es null, no necesita retornar nada
+ */
 public class ALUExecutedInstruction extends Event{
     public ALUExecutedInstruction(int cycles, Object[] info) {
         super(cycles, "Operacion ejecutada", info);

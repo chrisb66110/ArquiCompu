a03da5b0b820f0fc865c8018adfbce7b42148496
christofer <christofer.rodriguez@ucr.ac.cr>
2019-06-30 15:38:40 -0600

    CPUInterconnection, Fix number of LEVEL to the cacheWroteData event, and use conditional for generate event to next instruction

--- Simulacion/src/main/java/com/simulacion/CPUInterconnection.java
+++ Simulacion/src/main/java/com/simulacion/CPUInterconnection.java
@@ -8,7 +8,7 @@ import rx.Subscription;
  */
 public class CPUInterconnection {
     //Const
-    private final int LEVEL = -1; //Number to represent that the cache return data to the CPU.
+    private final int LEVEL = 0; //Number to represent that the cache return data to the CPU.
     private final int INFO_INDEX_LEVEL = 1; //Index of the level number in the info in event.
     private final int INFO_INDEX_DATA = 0; //Index of data in the info in event.
 
@@ -140,10 +140,12 @@ public class CPUInterconnection {
      */
     public void storeRegisterToMemory(ALUOperations operation, int registerResult, int registerIndex, BitsSet offset, OperandSize ammount){
         //This subscribe is waiting for a CacheWroteData event, so you know when the operation finished
-        this.cacheWroteData = rXBus.register(CacheWroteData.class, evento -> {
+        this.cacheWroteData = rXBus.register(CacheWroteData.class, event -> {
             //Event to execute next instruction
-            this.eventHandler.addEvent(new StartCUCycle(operation.cycles,null));
-            this.cacheWroteData.unsubscribe();
+            if(this.LEVEL == (int) event.info[Consts.INFO_LEVEL_INDEX-1]){
+                this.eventHandler.addEvent(new StartCUCycle(operation.cycles,null));
+                this.cacheWroteData.unsubscribe();
+            }
         });
         //Add offset and index from register
         BitsSet address = BitsSet.valueOf(offset.toInt());
